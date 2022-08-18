256b399dd078f0b8d439dd83649c9461eda8f61d
Gerson-GT <gerson.lopez.com@gmail.com>
2022-08-18 11:58:58 -0600

    06

--- install.sh
+++ install.sh
@@ -4,17 +4,24 @@
 #sudo apt upgrade
 #sudo apt autoremove
 
-echo "Instalando git"
-cd ~/
-sudo apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc
-mkdir tmp
-cd /tmp
-curl -o git.tar.gz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.37.2.tar.gz
-tar -zxf git.tar.gz
-cd git-*
-make prefix=/usr/local all
-sudo make prefix=/usr/local install
-#exec bash
-git --version
-cd ~/.dotfile/
+# echo "Instalando git"
+# cd ~/
+# sudo apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc
+# mkdir tmp
+# cd /tmp
+# curl -o git.tar.gz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.37.2.tar.gz
+# tar -zxf git.tar.gz
+# cd git-*
+# make prefix=/usr/local all
+# sudo make prefix=/usr/local install
+# #exec bash --suegen errores
+# git config --global user.name "Gerson-GT"
+# git config --global user.email "gerson.lopez.com@gmail.com"
+# git --version
+# git config --list
+# cd ~/.dotfile/
+
+# echo "Instalando Neovim"
+# sudo apt install -y vim neovim
+
 echo "Todo bien! Hasta luego"
