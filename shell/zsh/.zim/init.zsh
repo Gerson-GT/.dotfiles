e22c7723815da3ebea5659ad599a9333dee95628
Gerson-GT <gerson.lopez.com@gmail.com>
2024-05-03 21:19:05 -0600

    insert restorer

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,9 +1,2 @@
 zimfw() { source /home/gerson/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /home/gerson/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-fpath=(/home/gerson/.dotfiles/shell/zsh/.zim/modules/git-info/functions ${fpath})
-autoload -Uz -- coalesce git-action git-info
-source /home/gerson/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
-source /home/gerson/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
-source /home/gerson/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
-source /home/gerson/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /home/gerson/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
